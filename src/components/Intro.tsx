71499aa207e481ed314e37465095044340bfaa8d
Gonçalo Prates <g.prates@campus.fct.unl.pt>
2024-01-19 00:09:54 +0000

    added reference buttons

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
 
 const Intro = () => {
   return (
@@ -16,6 +19,7 @@ const Intro = () => {
           />
         </div>
       </div>
+
       <p className="mb-10 mt-4 px-4  text-2xl font-medium !leading-[1.5] sm:text-3xl">
         <span className="font-bold">Hello, my name is Gonçalo </span>
         and I have recently completed my Master&apos;s in{" "}
@@ -28,6 +32,42 @@ const Intro = () => {
         and
         <span className="italic underline"> NextJS or React</span>.
       </p>
+
+      <div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium">
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full border border-black/10
+            outline-none hover:scale-105 hover:bg-gray-950 active:scale-100 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a
+          href="/CV-EN.pdf"
+          download
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full cursor-pointer
+            outline-none  hover:scale-105 active:scale-100 transition"
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/goncaloprates/"
+          target="_blank"
+          className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full  cursor-pointer border border-black/10
+            outline-none hover:scale-[1.15] hover:text-gray-950 active:scale-100 transition"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/CptAllergy"
+          target="_blank"
+          className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full cursor-pointer border border-black/10
+            outline-none hover:scale-[1.15] hover:text-gray-950 active:scale-100 transition"
+        >
+          <BsGithub />
+        </a>
+      </div>
     </section>
   );
 };
